51a2a66d15ab89c6e31ba7184227895e808549d6
Viachaslav Suleimanov <sulvz2000@gmail.com>
2016-03-15 16:30:18 +0300

    Add Score column to Users table

--- app/api/api.rb
+++ app/api/api.rb
@@ -12,6 +12,8 @@ class API < Grape::API
 
   mount V1::Users
 
+  # SwaggerUI http://localhost:3000/api/swagger
+  # SwaggerUI path http://localhost:3000/api/v1/swagger_doc
   add_swagger_documentation api_version: 'v1',
                             hide_format: true,
                             hide_documentation_path: true
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160314090643) do
+ActiveRecord::Schema.define(version: 20160315131757) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20160314090643) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "auth_token"
+    t.integer  "score",                  default: 0
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
